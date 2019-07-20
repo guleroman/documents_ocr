a2c73287b1aff317d37be3abb636ef763f2fbad9
guleroman <remembername@protonmail.com>
2019-07-20 05:01:42 +0300

    init

--- app_2.py
+++ app_2.py
@@ -20,4 +20,4 @@ def document_ocr ():
 
 
 if __name__ == '__main__':
-    app.run(debug=False,threaded = True, host='0.0.0.0', port=80)
+    app.run(debug=False,threaded = True, host='0.0.0.0', port=2222)
